3e9504ac69ac05699e389aaf2d6658a037c73b35
Shahulabdullak <shahulabdullak@gmail.com>
2020-10-28 17:25:41 +0530

    Login & SignUp Layout

---
+++ app/src/main/java/com/frcbdemo/myblog/auth/SignUp.kt
@@ -0,0 +1,12 @@
+package com.frcbdemo.myblog.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.frcbdemo.myblog.R
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
