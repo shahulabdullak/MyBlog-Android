2e3fa779ea0b4cf0de745bb0c9f4f1ee833e1f0f
shahulabdulla <shahulabdullak@gmailk.com>
2020-10-30 22:42:06 +0530

    fist commit

--- app/src/main/java/com/frcbdemo/myblog/MainActivity.kt
+++ app/src/main/java/com/frcbdemo/myblog/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.frcbdemo.myblog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.frcbdemo.myblog.auth.SignUp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val SPLASH_TIME_OUT:Long=3000
+
+        Handler().postDelayed({
+        startActivity(Intent(this,SignUp::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
+
+
+
     }
 }
